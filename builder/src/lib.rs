819c51ef87d29ef8bfa13d366701affa0c789fdd
yue4u <github@yue.coffee>
2024-02-04 02:33:27 +0900

    wip: builder/02-create-builder

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,9 +1,51 @@
 use proc_macro::TokenStream;
-use syn::{parse_macro_input, DeriveInput};
+use quote::quote;
+use syn::{parse_macro_input, spanned::Spanned, DeriveInput, Ident};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = parse_macro_input!(input as DeriveInput);
+    let input = parse_macro_input!(input as DeriveInput);
 
-    TokenStream::new()
+    let builder_ident = Ident::new(&format!("{}Builder", &input.ident), input.span());
+    let input_ident = input.ident;
+
+    let gen = match &input.data {
+        syn::Data::Struct(s) => {
+            let mut option_fields = vec![];
+            let mut none_fields = vec![];
+
+            s.fields.iter().for_each(|f| {
+                let Some(ident) = f.ident.as_ref() else {
+                    return;
+                };
+                let ty = &f.ty;
+                option_fields.push(quote!( #ident: Option<#ty>));
+                none_fields.push(quote!( #ident: None));
+            });
+
+            let _struct = quote!(
+                pub struct #builder_ident {
+                    #(#option_fields),*
+                }
+            );
+
+            let _impl = quote!(
+                impl #input_ident {
+                    pub fn builder() -> #builder_ident {
+                        #builder_ident {
+                            #(#none_fields),*
+                        }
+                    }
+                }
+            );
+
+            quote!(
+                #_struct
+                #_impl
+            )
+        }
+        _ => quote!(),
+    };
+
+    gen.into()
 }
