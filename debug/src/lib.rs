924c94d3f4380946f130d0d7d85ae4a5d22d2cc2
yue4u <github@yue.coffee>
2024-02-04 21:21:09 +0900

    wip: debug/02-impl-debug

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,8 +1,34 @@
 use proc_macro::TokenStream;
 use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(CustomDebug)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
-    quote!().into()
+    let input = parse_macro_input!(input as DeriveInput);
+    let ident_lit = input.ident.to_string();
+    let ident = input.ident;
+
+    let fields = match &input.data {
+        syn::Data::Struct(s) => s.fields.iter().filter_map(|f| {
+            let field_name = f.ident.as_ref()?;
+            let field_name_lit = field_name.to_string();
+            Some(quote!(
+                .field(#field_name_lit, &self.#field_name)
+            ))
+        }),
+        syn::Data::Enum(_) => unimplemented!(),
+        syn::Data::Union(_) => unimplemented!(),
+    };
+
+    quote!(
+        impl std::fmt::Debug for #ident {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                f.debug_struct(#ident_lit)
+                    #(#fields)*
+                    .finish()
+            }
+        }
+
+    )
+    .into()
 }
--- main.rs
+++ main.rs
@@ -5,16 +5,12 @@
 //
 // To run the code:
 //     $ cargo run
-use derive_builder::Builder;
+use derive_debug::CustomDebug;
 
-#[derive(Builder)]
-pub struct Command {
-    executable: String,
-    #[builder(each = "arg")]
-    args: Vec<String>,
-    #[builder(each = "env")]
-    env: Vec<String>,
-    current_dir: Option<String>,
+#[derive(CustomDebug)]
+pub struct Field {
+    name: &'static str,
+    bitmask: u8,
 }
 
 fn main() {}
