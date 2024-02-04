a59a93c8c37a255dbabbc5f7d51df01e1fe17c1b
yue4u <github@yue.coffee>
2024-02-04 22:07:37 +0900

    wip: debug/03-custom-format

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,8 +1,8 @@
 use proc_macro::TokenStream;
-use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
+use quote::{quote, ToTokens};
+use syn::{parse_macro_input, DeriveInput, ExprLit, Lit, MetaNameValue};
 
-#[proc_macro_derive(CustomDebug)]
+#[proc_macro_derive(CustomDebug, attributes(debug))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let ident_lit = input.ident.to_string();
@@ -12,8 +12,30 @@ pub fn derive(input: TokenStream) -> TokenStream {
         syn::Data::Struct(s) => s.fields.iter().filter_map(|f| {
             let field_name = f.ident.as_ref()?;
             let field_name_lit = field_name.to_string();
+
+            let fmt = f.attrs.iter().find_map(|attr| {
+                if attr.path().is_ident("debug") {
+                    if let Ok(MetaNameValue {
+                        value:
+                            syn::Expr::Lit(ExprLit {
+                                lit: Lit::Str(lit_str),
+                                ..
+                            }),
+                        ..
+                    }) = attr.meta.require_name_value()
+                    {
+                        return Some(lit_str.value().to_token_stream());
+                    }
+                }
+                None
+            });
+
+            let display = match fmt {
+                Some(fmt) => quote!(&format_args!(#fmt, &self.#field_name)),
+                None => quote!(&self.#field_name),
+            };
             Some(quote!(
-                .field(#field_name_lit, &self.#field_name)
+                .field(#field_name_lit, #display)
             ))
         }),
         syn::Data::Enum(_) => unimplemented!(),
