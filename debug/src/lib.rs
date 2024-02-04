d2a13e5828b76fb9b48c7339006f470392b47967
yue4u <github@yue.coffee>
2024-02-04 21:09:47 +0900

    wip: debug/01-parse

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,8 +1,8 @@
 use proc_macro::TokenStream;
+use quote::quote;
 
 #[proc_macro_derive(CustomDebug)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let _ = input;
-
-    unimplemented!()
+    quote!().into()
 }
